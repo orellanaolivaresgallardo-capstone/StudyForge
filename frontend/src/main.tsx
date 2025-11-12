b055e72b91f9ec6e86ac8cfc542506c7ac52186c
Sebastián Nicolás Gallardo González <106987930+sebgallardog@users.noreply.github.com>
2025-11-12 18:13:03 -0300

    Revert "Update main.tsx"
    This reverts commit fa20c04da9445c60d1128930268168e6bc0b5d6f.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -26,6 +26,10 @@ const router = createBrowserRouter([
 ]);
 
 if (rootEl) {
+  const landingEl = document.getElementById("landing");
+  if (landingEl) {
+    landingEl.style.display = "none";
+  }
   rootEl.style.display = "block";
   createRoot(rootEl).render(
     <StrictMode>
