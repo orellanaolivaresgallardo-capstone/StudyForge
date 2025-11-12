ffefc330a8444972fb3f2bc58ff0d4db6b87945b
Sebastián Nicolás Gallardo González <106987930+sebgallardog@users.noreply.github.com>
2025-11-12 10:08:47 -0300

    Revert "Add separate tsconfig files for app and node, update ESLint config"
    This reverts commit 4ebe7d7c90cabe5d4d88e6650a476ab4b0a7b8f8.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -11,15 +11,11 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommendedTypeChecked,
+      tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      parserOptions: {
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
-        tsconfigRootDir: import.meta.dirname,
-      },
       ecmaVersion: 2020,
       globals: globals.browser,
     },
